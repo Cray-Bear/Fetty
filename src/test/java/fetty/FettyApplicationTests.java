f775c6d100c3318feed713d54b8790362f678ec5
24KBear <59152642+24KBear@users.noreply.github.com>
2022-10-24 23:45:45 +0800

    Initial commit

---
+++ src/test/java/fetty/FettyApplicationTests.java
@@ -0,0 +1,13 @@
+package fetty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FettyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
